14e148101d08a8fee37bc59a28d681e4fcb240f5
Ravindra Devrani <ravindra.devrani003@github.com>
2026-08-30 21:15:42 +0530

    feat: create ReadBookDto and CreateBookDto

---
+++ BookShop.Api/Models/DTOs/CreateBookDto.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Api.Models.DTOs;
+
+public class CreateBookDto
+{
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string Description { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(20)]
+    public string Isbn { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public string CoverImageUrl { get; set; } = string.Empty;
+
+    public int? PublisherId { get; set; }
+
+    // In case, if you want to create a new publisher
+    public string? PublisherName { get; set; }
+
+    public IEnumerable<CreateGenreDto> Genres { get; set; } = [];
+    public IEnumerable<CreateGenreDto> Author { get; set; } = [];
+}
---
+++ BookShop.Api/Models/DTOs/ReadBookDto.cs
@@ -0,0 +1,19 @@
+namespace BookShop.Api.Models.DTOs;
+
+public class ReadBookDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Isbn { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int StockQuantity { get; set; }
+    public string CoverImageUrl { get; set; } = string.Empty;
+
+    public int PublisherId { get; set; }
+
+    public string PublisherName { get; set; } = string.Empty;
+    public IEnumerable<ReadGenreDto> Genres { get; set; } = [];
+    public IEnumerable<ReadGenreDto> Authors { get; set; } = [];
+
+}
