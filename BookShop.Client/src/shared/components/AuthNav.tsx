5fbe94b75f6cd4e1d63af0277aeafdad353c32fe
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-10 17:10:28 +0530

    feat: add logout in frontend

--- BookShop.Client/src/shared/components/AuthNav.tsx
+++ BookShop.Client/src/shared/components/AuthNav.tsx
@@ -1,21 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { User } from "lucide-react";
 import { useUser } from "@/auth/hooks/useUser";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout } from "@/auth/api/authApi";
 
 export default function AuthNav() {
     const { data: user, isLoading } = useUser();
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+    const logoutMutation = useMutation({
+        mutationFn: logout,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['user'] });
+            navigate('/');
+        }
+    })
+
+    function handleLogoutClick() {
+        logoutMutation.mutate();
+    }
 
     if (isLoading) return null;
 
     if (user) {
         return (
-            <>
-                <Button render={<Link to="/account" />} variant="ghost" size="icon" aria-label="Account" nativeButton={false}>
-                    <User className="h-5 w-5 text-stone-700" />
+            <div className="hidden md:flex items-center gap-2 ml-2">
+                <div className="md:flex items-center gap-0.5">
+                    <Button render={<Link to="/account" />} variant="ghost" size="icon" aria-label="Account" nativeButton={false}>
+                        <User className="h-5 w-5 text-stone-700" />
+                    </Button>
+                    <span className="text-sm text-stone-700 block">{user.username}</span>
+                </div>
+                <Button
+                    variant="default"
+                    size="sm"
+                    onClick={handleLogoutClick}
+                    disabled={logoutMutation.isPending}
+                >
+                    Log out
                 </Button>
-                <span>({user.username})</span>
-            </>
+            </div>
         );
     }
 
