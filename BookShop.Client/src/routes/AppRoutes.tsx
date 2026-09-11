7aa6fe529eaba786c22c5b5906df5be6b0594dff
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-11 16:20:11 +0530

    refactor: routes are extracted from App.tsx to AppRoutes.tsx

---
+++ BookShop.Client/src/routes/AppRoutes.tsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import RequireAuth from "./RequireAuth";
+import Account from "@/auth/Account";
+import About from "@/pages/About";
+import TermsOfService from "@/pages/TermsOfService";
+import ContactUs from "@/pages/ContactUs";
+import PrivacyPolicy from "@/pages/PrivacyPolicy";
+import Careers from "@/pages/Careers";
+import Signup from "@/pages/Signup";
+import Books from "@/books/Books";
+import Login from "@/auth/Login";
+import Cart from "@/cart/Cart";
+import NotFound from "@/pages/NotFound";
+import Layout from "@/shared/ui/Layout";
+
+export default function AppRoutes() {
+    return (<Routes>
+        <Route element={<Layout />}>
+            <Route index element={<Books />} />
+            <Route path="catalog" element={<Books />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="privacy" element={<PrivacyPolicy />} />
+            <Route path="contact" element={<ContactUs />} />
+            <Route path="careers" element={<Careers />} />
+            <Route path="terms" element={<TermsOfService />} />
+            <Route path="about" element={<About />} />
+            <Route element={<RequireAuth />}>
+                <Route path="account" element={<Account />} />
+                <Route path="cart" element={<Cart />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+        </Route>
+    </Routes>);
+}
