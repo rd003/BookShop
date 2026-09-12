d93f90be16bf5cd0477b6298ef6190b5e9efc0e9
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-12 17:36:03 +0530

    feat: create addressApi

---
+++ BookShop.Client/src/addresses/apis/addressApi.ts
@@ -0,0 +1,32 @@
+import { apiFetch } from "@/lib/apiClient";
+import type { ReadAddress } from "../types/readAddress";
+import type { CreateAddress } from "../types/createAddress";
+import type { UpdateAddress } from "../types/updateAddress";
+
+const url = "/addresses";
+
+export async function getAddress(): Promise<ReadAddress[]> {
+    const addresses = await apiFetch<ReadAddress[]>(url);
+    return addresses;
+}
+
+export async function createAddress(createAddress: CreateAddress): Promise<ReadAddress> {
+    const createdAddress = await apiFetch<ReadAddress>(url, {
+        method: 'POST',
+        body: JSON.stringify(createAddress)
+    });
+    return createdAddress;
+}
+
+export async function updateAddress(updateAddress: UpdateAddress): Promise<void> {
+    await apiFetch<ReadAddress>(url, {
+        method: 'PATCH',
+        body: JSON.stringify(updateAddress)
+    });
+}
+
+export async function deleteAddress(addressId: number): Promise<void> {
+    await apiFetch<ReadAddress>(url + "/" + addressId, {
+        method: 'DELETE',
+    });
+}
