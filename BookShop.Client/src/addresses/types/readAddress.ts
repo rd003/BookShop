e2364fb2b1b2d117131036f52eeada6b1ee7050e
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-12 16:54:44 +0530

    feat: add createAddress, updateAddress and readAddress types

---
+++ BookShop.Client/src/addresses/types/readAddress.ts
@@ -0,0 +1,12 @@
+export interface ReadAddress {
+    id: number;
+    fullName: string;
+    phone: string;
+    line1: string;
+    line2: string | null;
+    city: string;
+    state: string;
+    postalCode: string;
+    country: string;
+    isDefault: boolean;
+}
