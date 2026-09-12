81fd5021fa7e71710411471a4a94374fcdf24baf
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-12 12:56:02 +0530

    feat: add cart is successfull

--- BookShop.Client/src/App.tsx
+++ BookShop.Client/src/App.tsx
@@ -1,9 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
+import { Toaster } from "@/components/ui/toast"
 
 export default function App() {
   return (
     <>
       <AppRoutes />
+      <Toaster />
     </>
   )
 }
--- BookShop.Client/src/shared/components/CartIcon.tsx
+++ BookShop.Client/src/shared/components/CartIcon.tsx
@@ -2,9 +2,10 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "lucide-react";
+import { useCartCount } from "@/cart/hooks/useCartCount";
 
 export default function CartIcon() {
-    const cartCount = 0;
+    const cartCount = useCartCount();
     return (<Button
         variant="ghost"
         size="icon"
