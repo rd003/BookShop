bf747f8c1023887228b9e13f2179c06e1881b70a
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-14 16:29:25 +0530

    feat: add address form and dialog UI

--- BookShop.Client/src/addresses/AddressDialog.tsx
+++ BookShop.Client/src/addresses/AddressDialog.tsx
@@ -1,23 +1,39 @@
+import { Button } from "@/components/ui/button";
 import {
     Dialog,
+    DialogClose,
     DialogContent,
-    DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import { AddressForm } from "./AddressForm";
 
-export default function AddressDialog() {
+interface AddressDialogProps {
+    title: string;
+    isSubmitting: boolean;
+    submitLabel: string;
+    dialogButtonTitle: string
+}
+
+export default function AddressDialog({ title = "Add new address", isSubmitting = false, submitLabel = "Add", dialogButtonTitle = "Add +" }: AddressDialogProps) {
     return (<Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
+        <DialogTrigger render={<Button variant="outline" />}>{dialogButtonTitle}</DialogTrigger>
+        <DialogContent className="flex max-h-[90vh] flex-col sm:max-w-2xl">
             <DialogHeader>
-                <DialogTitle>Are you absolutely sure?</DialogTitle>
-                <DialogDescription>
-                    This action cannot be undone. This will permanently delete your account
-                    and remove your data from our servers.
-                </DialogDescription>
+                <DialogTitle>{title}</DialogTitle>
             </DialogHeader>
+            <div className="-mx-6 flex-1 overflow-y-auto px-6">
+                <AddressForm
+                    onSubmit={(val) => console.log(val)}
+                    isSubmitting={isSubmitting}
+                    submitLabel={submitLabel}
+                />
+            </div>
+            <DialogFooter className="sm:justify-start">
+                <DialogClose render={<Button type="button">Close</Button>} />
+            </DialogFooter>
         </DialogContent>
-    </Dialog>);
+    </Dialog >);
 }
