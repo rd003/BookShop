658d16801f243bb43ebe8b7937d648cb09b4e4a0
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-18 16:16:14 +0530

    feat: date filters for user orders

--- BookShop.Client/src/orders/Orders.tsx
+++ BookShop.Client/src/orders/Orders.tsx
@@ -1,8 +1,9 @@
 import type { OrdersQueryParameters } from "@/shared/types/queryParameters";
 import { useOrders } from "./hooks/useOrders"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getUserFacingError } from "@/lib/getUserFacingError";
 import { formatDateTime } from "@/lib/format";
+import OrderFilters, { type OrderFilter } from "./OrderFilters";
 
 export default function Orders() {
     const queryParams: OrdersQueryParameters = {
@@ -14,6 +15,25 @@ export default function Orders() {
     const [endDate, setEndDate] = useState<string | null>(null);
     const { data, status, error } = useOrders(queryParams, startDate, endDate);
 
+    useEffect(() => {
+        console.log({ startDate, endDate });
+    }, [startDate, endDate])
+
+    function handleOrderFilterClick(filterValues: OrderFilter) {
+        const { dateFrom, dateTo } = filterValues;
+        if (dateFrom) {
+            setStartDate(dateFrom.toISOString());
+        }
+        if (dateTo) {
+            setEndDate(dateTo.toISOString());
+        }
+    }
+
+    function handleCrearFilter() {
+        setStartDate(null);
+        setEndDate(null);
+    }
+
     if (status === 'pending') {
         return (<p>Loadin</p>)
     }
@@ -22,11 +42,19 @@ export default function Orders() {
     }
     return (<>
         <h1 className="text-3xl">Orders</h1>
+
+        <OrderFilters
+            onClick={handleOrderFilterClick}
+            onClearFilter={handleCrearFilter}
+        />
         <ul>
             {data.items.map(o => <li key={o.orderNumber}>
                 {formatDateTime(o.orderDate)}
                 | {o.orderNumber}
                 | {o.orderStatus}
+                | {o.paymentMethod}
+                | {o.paymentStatus}
+                | {o.orderTotal}
             </li>)}
         </ul>
     </>)
