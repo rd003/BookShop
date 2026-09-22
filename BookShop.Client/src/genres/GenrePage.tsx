61c37ead1e756af8063a7c607d95910a92a3d501
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-22 18:33:07 +0530

    WIP: Genre form

--- BookShop.Client/src/genres/GenrePage.tsx
+++ BookShop.Client/src/genres/GenrePage.tsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+import type { UpdateGenre } from "./types/updateGenre";
+import GenreForm from "./ui/GenreForm";
+
+
 export default function GenrePage() {
-    return (<p>Genres...</p>)
+    const [selectedGenre, setSelectedGenre] = useState<UpdateGenre | null>(null);
+    const submitting: boolean = false;
+
+    function handleSubmit(genre: UpdateGenre) {
+        console.log(genre);
+        setSelectedGenre(null);
+    }
+
+    return (<div>
+        <h1 className="text-xl">Genre</h1>
+
+        <GenreForm
+            selectedGenre={selectedGenre}
+            onSubmit={handleSubmit}
+            submitting={submitting}
+        />
+    </div>)
 }
---
+++ BookShop.Client/src/genres/ui/GenreForm.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import type { UpdateGenre } from "../types/updateGenre";
+
+interface Props {
+    selectedGenre: UpdateGenre | null,
+    onSubmit: (genre: UpdateGenre) => void,
+    submitting: boolean
+}
+
+export default function GenreForm({
+    selectedGenre = null,
+    onSubmit,
+    submitting = false
+}: Props) {
+    const [name, setName] = useState<string>('');
+    const [validationError, setValidationError] = useState<string | null>();
+    const [id, setId] = useState<number | undefined>(undefined);
+
+    useEffect(() => {
+        console.log("use effect in genreForm");
+        console.log(selectedGenre);
+        if (selectedGenre) {
+            setId(selectedGenre.id);
+            setName(selectedGenre.name);
+        }
+        else {
+            handleReset();
+        }
+    }, [selectedGenre])
+
+    function handleReset() {
+        setName('');
+        setId(undefined);
+    }
+
+    function handleSubmit(e: React.SubmitEvent) {
+        e.preventDefault();
+        if (!isNameValid(name)) return;
+        onSubmit({ id, name } as UpdateGenre)
+    }
+
+    function isNameValid(val: string): boolean {
+        if (val === null || val.trim().length == 0) {
+            setValidationError("Name can not be empty");
+            return false;
+        }
+        if (val !== null && val.length >= 100) {
+            setValidationError("Name can not exceed 100 characters");
+            return false;
+        }
+        return true;
+    }
+
+    return (
+        <form className="flex gap-1" onSubmit={(e) => handleSubmit(e)}>
+            <input type="hidden" value={id} />
+            <div className="flex gap-1">
+                <label htmlFor="name">Name</label>
+                <Input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                {validationError && <p className="text-red-500">{validationError}</p>}
+            </div>
+            <Button type="reset" variant="outline" onClick={handleReset}>Reset</Button>
+            <Button type="submit" variant="default" disabled={submitting}>Save</Button>
+        </form>
+    )
+}
