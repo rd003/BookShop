a88643fd6c649ff4ae70fe7c9b487e4b39cd7025
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-22 17:06:59 +0530

    feat: add remaining genreApi methods

--- BookShop.Client/src/genres/genreApi.ts
+++ BookShop.Client/src/genres/genreApi.ts
@@ -2,7 +2,10 @@ import type { PagedList } from "@/shared/types/pagedList";
 import type { QueryParameters } from "@/shared/types/queryParameters";
 import type { ReadGenre } from "./types/readGenre";
 import { apiFetch } from "@/lib/apiClient";
+import type { CreateGenre } from "./types/createGenre";
+import type { UpdateGenre } from "./types/updateGenre";
 
+const url = "/genres";
 export async function getGenres(queryParams: QueryParameters): Promise<PagedList<ReadGenre>> {
     const params = new URLSearchParams({
         pageNumber: String(queryParams.pageNumber),
@@ -13,5 +16,25 @@ export async function getGenres(queryParams: QueryParameters): Promise<PagedList
         params.set("sortBy", queryParams.sortBy);
     }
 
-    return apiFetch<PagedList<ReadGenre>>(`/genres?${params.toString()}`);
+    return await apiFetch<PagedList<ReadGenre>>(`${url}?${params.toString()}`);
+}
+
+export async function createGenre(genre: CreateGenre): Promise<ReadGenre> {
+    return await apiFetch<ReadGenre>(url, {
+        method: 'POST',
+        body: JSON.stringify(genre)
+    })
+}
+
+export async function updateGenre(genre: UpdateGenre): Promise<void> {
+    await apiFetch<void>(url + "/" + genre.id, {
+        method: 'PUT',
+        body: JSON.stringify(genre)
+    })
+}
+
+export async function deleteGenre(id: number): Promise<void> {
+    await apiFetch<void>(url + "/" + id, {
+        method: 'DELETE'
+    })
 }
