5639d7a06ad32f472228e0d19565ed1e91ff1f76
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-23 16:45:04 +0530

    feat: add genre filter

---
+++ BookShop.Client/src/genres/ui/GenreFilter.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "cn";
+import { useState } from "react";
+
+interface Props {
+    className?: string;
+    onSearch: (term: string) => void;
+    onClear: () => void;
+}
+export default function GenreFilter({
+    className,
+    onSearch,
+    onClear
+}: Props) {
+    const [term, setTerm] = useState<string>('');
+
+    function handleSubmit(e: React.SubmitEvent) {
+        e.preventDefault();
+        if (term.trim()) {
+            onSearch(term);
+        }
+    }
+
+    function handleClear() {
+        setTerm('');
+        onClear();
+    }
+
+    return (
+        <form className={cn("w-full flex gap-2 align-middle justify-start", className)} onSubmit={handleSubmit}>
+            <Input type="text" className="w-60" value={term} onChange={(e) => setTerm(e.target.value)} placeholder="search by genre" />
+
+            <Button type="submit" variant="default">Search</Button>
+
+            <Button type="button" variant="secondary" onClick={handleClear}>Clear</Button>
+        </form>)
+}
