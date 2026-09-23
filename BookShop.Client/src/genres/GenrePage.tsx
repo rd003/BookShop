41d3f764f3a40ca81ebaa4d4322537b825a4ccff
Ravindra Devrani <ravindra.devrani003@github.com>
2026-09-23 10:53:43 +0530

    feat: complete genre form ui

--- BookShop.Client/src/genres/GenrePage.tsx
+++ BookShop.Client/src/genres/GenrePage.tsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import type { UpdateGenre } from "./types/updateGenre";
 import GenreForm from "./ui/GenreForm";
 
-
 export default function GenrePage() {
     const [selectedGenre, setSelectedGenre] = useState<UpdateGenre | null>(null);
+    const [resetSignal, setResetSignal] = useState<number>(0);
+
     const submitting: boolean = false;
 
     function handleSubmit(genre: UpdateGenre) {
         console.log(genre);
+        setResetSignal(prev => prev + 1);
         setSelectedGenre(null);
+    }
+
+    function handleFormEdit(genre: UpdateGenre) {
+        setSelectedGenre(genre);
     }
 
     return (<div>
@@ -19,6 +25,7 @@ export default function GenrePage() {
             selectedGenre={selectedGenre}
             onSubmit={handleSubmit}
             submitting={submitting}
+            resetSignal={resetSignal}
         />
     </div>)
 }
